7c0513e00d7ea4f61c26b8ecb95a93b8ada488ca
ProgrammerJayT <theanthem8@gmail.com>
2023-06-26 13:59:26 +0200

    Created book images entity

---
+++ database/migrations/2023_06_26_115726_create_book_images_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_images', function (Blueprint $table) {
+            $table->integer('book_image_id')->autoIncrement();
+            $table->integer('book_id'); //Foreign key
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_images');
+    }
+};
